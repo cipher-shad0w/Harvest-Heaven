a5703f985a207a73f7411d99904d11e0c07cf390
Cipher Shad0w <Jannis.krija@icloud.com>
2025-03-13 11:32:13 +0100

    add comments and grow effect, and plant to item transform without item

--- Assets/Scripts/FarmTile.cs
+++ Assets/Scripts/FarmTile.cs
@@ -1,16 +1,25 @@
+using UnityEngine;
+
 public class FarmTile : MonoBehaviour
 {
+    // flag to check if the tile is already planted
     public bool isPlanted = false;
-    public GameObject plantPrefab; // Prefab mit "Plant.cs"
+
+    // reference to the plant prefab
+    public GameObject plantPrefab;
 
     public void PlantSeed()
     {
         if (!isPlanted)
         {
+            // get the center of the collider bounds
             Vector3 spawnPosition = GetComponent<Collider2D>().bounds.center;
-            spawnPosition.z = 0; 
+            spawnPosition.z = 0;
 
+            // instantiate the plant prefab
             Instantiate(plantPrefab, spawnPosition, Quaternion.identity);
+
+            // set the flag to true
             isPlanted = true;
         }
     }
